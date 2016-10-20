0f86e76615b786c8a69214e434f5e72fb7a87415
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-10-20 12:56:01 -0500

    inicio

---
+++ js/app.js
@@ -0,0 +1,205 @@
+window.addEventListener("load", cargaPagina);
+
+	var cajaSelector = document.getElementById("cajaSelector");
+	var selector = document.getElementById("selector");
+	var contenedorOpcion = document.createElement("div");
+	var contenedorPublicacion = document.createElement("div");
+	function cargaPagina (){
+		//selector.addEventListener("change", seleccionarOpcion);
+		$("#selector").on("change", seleccionarOpcion);
+	}
+
+	function seleccionarOpcion (){
+		var selectorValor = selector.value;
+		switch(selectorValor){
+			case "texto":
+				texto();
+
+				break;
+			case "cita":
+				cita();					
+				break;
+			case "meme":
+				meme();						
+				break;
+			case "pastilla":
+				pastilla();							
+				break;
+		}
+	}
+
+	function texto(){
+		var contenedor = document.createElement("div");
+		var textoInput = document.createElement("input");
+		var textareaTexto  = document.createElement("textarea");
+		var botonCerrar = document.createElement("button");
+		var botonPublicar = document.createElement("button");
+		
+		contenedor.setAttribute("id", "contenedor");
+		textoInput.setAttribute("placeholder","Titulo");
+		textareaTexto.setAttribute("placeholder","Texto");
+		
+		botonCerrar.textContent = "Cerrar";
+		botonPublicar.textContent = "publicar";
+
+		textoInput.classList.add("textoInput");
+		textareaTexto.classList.add("textareaTexto");
+		botonCerrar.classList.add("waves-effect","waves-light","btn");
+		botonPublicar.classList.add("waves-effect","waves-light","btn");
+		
+		cajaSelector.insertBefore(contenedorOpcion, cajaSelector.children[1]);
+		contenedorOpcion.appendChild(contenedor);
+		contenedor.appendChild(textoInput);
+		contenedor.appendChild(textareaTexto);
+		contenedor.appendChild(botonCerrar);
+		contenedor.appendChild(botonPublicar);
+
+
+		botonPublicar.addEventListener("click", publicar);
+		botonCerrar.addEventListener("click", cerrar);
+	}
+	function cita() {
+		var contenedor = document.createElement("div");
+		var textareaTexto  = document.createElement("textarea");
+		var textoInput = document.createElement("input");
+		var botonCerrar = document.createElement("button");
+		var botonPublicar = document.createElement("button");
+		
+		contenedor.setAttribute("id", "contenedor");
+		textareaTexto.setAttribute("placeholder","Texto");
+		textoInput.setAttribute("placeholder","Autor");
+		
+		botonCerrar.textContent = "Cerrar";
+		botonPublicar.textContent = "Publicar";
+
+		textareaTexto.classList.add("textareaTexto");
+		textoInput.classList.add("textoInput");
+		botonCerrar.classList.add("waves-effect","waves-light","btn");
+		botonPublicar.classList.add("waves-effect","waves-light","btn");
+		
+
+		cajaSelector.insertBefore(contenedorOpcion,cajaSelector.children[1]);
+		contenedorOpcion.appendChild(contenedor);
+		contenedor.appendChild(textoInput);
+		contenedor.appendChild(textareaTexto);
+		contenedor.appendChild(botonCerrar);
+		contenedor.appendChild(botonPublicar);
+
+
+		botonPublicar.addEventListener("click", publicar);
+		botonCerrar.addEventListener("click", cerrar);
+	}
+	function meme() {
+		var contenedor = document.createElement("div");
+		var textoInput  = document.createElement("input");
+		var botonCerrar = document.createElement("button");
+		var botonPublicar = document.createElement("button");
+		
+		contenedor.setAttribute("id", "contenedor");
+		textoInput.setAttribute("placeholder","link");
+		
+		botonCerrar.textContent = "Cerrar";
+		botonPublicar.textContent = "Publicar";
+
+		textoInput.classList.add("textoInput");
+		botonCerrar.classList.add("waves-effect","waves-light","btn");
+		botonPublicar.classList.add("waves-effect","waves-light","btn");
+
+		contenedorOpcion.appendChild(contenedor);
+		cajaSelector.insertBefore(contenedorOpcion,cajaSelector.children[1]);
+		contenedor.appendChild(textoInput);
+		contenedor.appendChild(botonCerrar);
+		contenedor.appendChild(botonPublicar);
+
+
+		botonPublicar.addEventListener("click", imagen);
+		botonCerrar.addEventListener("click", cerrar);
+	}
+	function pastilla() {
+		var contenedor = document.createElement("div");
+		var textareaTexto  = document.createElement("textarea");
+		var textoInput = document.createElement("input");
+		var botonCerrar = document.createElement("button");
+		var botonPublicar = document.createElement("button");
+		
+		contenedor.setAttribute("id", "contenedor");
+		textareaTexto.setAttribute("placeholder","Texto");
+		textoInput.setAttribute("type","color");
+		
+		botonCerrar.textContent = "Cerrar";
+		botonPublicar.textContent = "Publicar";
+
+		textareaTexto.classList.add("textareaTexto");
+		textoInput.classList.add("textoInput");
+		botonCerrar.classList.add("waves-effect","waves-light","btn");
+		botonPublicar.classList.add("waves-effect","waves-light","btn");
+		
+		cajaSelector.insertBefore(contenedorOpcion,cajaSelector.children[1]);
+		contenedorOpcion.appendChild(contenedor);
+		contenedor.appendChild(textareaTexto);
+		contenedor.appendChild(textoInput);
+		contenedor.appendChild(botonCerrar);
+		contenedor.appendChild(botonPublicar);
+
+		botonPublicar.addEventListener("click", publicar);
+		botonCerrar.addEventListener("click", cerrar);
+	}
+
+	function cerrar () {
+		this.parentElement.classList.add("ocultar");
+
+	}
+
+	function publicar () {
+		var h2 = document.createElement("h2");
+		var p = document.createElement("p");
+		var divPub = document.createElement("div");
+		divPub.setAttribute("id", "foto");
+		var divHora = document.createElement("div");
+		var horaPub = fechaPub();
+		var botonEliminar = document.createElement("button");
+
+		var titulo = this.parentElement.children[0].value;
+		var texto = this.parentElement.children[1].value;
+
+		h2.innerText = titulo;
+		p.innerText = texto;
+		divHora.innerText = horaPub;
+		botonEliminar.textContent = "Eliminar";
+
+		cajaSelector.appendChild(contenedorPublicacion);
+		contenedorPublicacion.appendChild(divPub);
+		divPub.appendChild(h2);
+		divPub.appendChild(p);
+		divPub.appendChild(divHora);
+		divPub.appendChild(botonEliminar);
+
+		botonEliminar.addEventListener("click", eliminar);
+	}
+	function imagen() {
+		 var inputUrl = document.createElement("img");
+            inputUrl.setAttribute("src",this.parentElement.children[0].value);
+            cajaSelector.appendChild(contenedorPublicacion);
+            contenedorPublicacion.appendChild(inputUrl);
+	}
+	function eliminar () {
+		this.parentElement.remove();
+	}
+
+	function fechaPub () {
+		var fecha = new Date();
+		var hora = fecha.getHours();
+		var minuto = fecha.getMinutes();
+		var segundo = fecha.getSeconds();
+		return hora + ":" + minuto + ":" + segundo;
+	}
+	function limpiarCampos () {
+	}
+
+	function crearElementos () {
+	}
+
+	function validarCampos (){
+	}
+
+
---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(function () {
+$('select').material_select();
+});
+
+$(".button-collapse").sideNav();
+
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea1').characterCounter();
+  });
+
+// $('input.autocomplete').autocomplete({
+//     data: {
+//       "Apple": null,
+//       "Microsoft": null,
+//       "Google": 'http://placehold.it/250x250'
+//     }
+//   });
+      
